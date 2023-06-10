76584bed5bbe904fc7396ce41fcb5cc3bcb8175a
PRO-PYTHON-X <121113802+HARSH-PYTHON-X@users.noreply.github.com>
2023-06-10 14:19:46 +0530

    Create HARSH* PATHFINDING.py
    PATHFINDING ALGORITHM by HARSH , make sure to use GRID.png provided in repository

---
+++ HARSH* PATHFINDING.py
@@ -0,0 +1,53 @@
+while True :
+
+    x_target = int(input('targets x axis >'))
+    y_target = int(input('targets y axis >'))
+
+    hunter_x, hunter_y = 0, 0
+
+    print()
+
+    print(f'straight :')
+
+    print(f'left {x_target}')
+    print(f'down {y_target}')
+
+    print()
+
+    if x_target >= 5 and x_target >= y_target :
+
+        print(f'diagonally :')
+
+        print(f'diagonally down left {y_target}')
+        print(f'left {x_target - y_target}')
+
+        print()
+
+    elif x_target >= 5 and x_target <= y_target :
+
+        print(f'diagonally :')
+
+        print(f'diagonally down left {x_target}')
+        print(f'down {y_target - x_target}')
+
+        print()
+
+
+
+    elif x_target <= 4 and x_target >= y_target :
+
+        print(f'diagonally :')
+
+        print(f'diagonally down left {y_target}')
+        print(f'left {x_target - y_target}')
+
+        print()
+
+    elif x_target <= 4 and x_target <= y_target :
+
+        print('diagonally :')
+
+        print(f'diagonally down left {x_target}')
+        print(f'down {y_target - x_target}')
+
+        print()
